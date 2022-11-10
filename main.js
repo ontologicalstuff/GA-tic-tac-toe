e0ec9335069c682a5a0a00ecd03bffc09d2951de
ontologicalstuff <saorsa@tutamail.com>
2022-11-10 13:46:10 -0700

    set turn variable and set conditions for board cell to populate x or o and set change of gameboard index

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 // constants 
+
+const gameBoard = [ 
+    0, 0, 0, 
+    0, 0, 0, 
+    0, 0, 0
+];
+
+let turn = 1;
+
 const cellIds = document.querySelectorAll('div');
 
 cellIds.forEach((e) => {
     e.onclick = (e) => {
         const cellId = document.querySelector(`#${e.currentTarget.id}`);
-        cellId.textContent = 'X';
+        if (turn % 2 === 1) {
+          cellId.textContent = 'X';
+        } else {
+          cellId.textContent = 'O'
+        }      
+        gameBoard[parseFloat(e.currentTarget.id[e.currentTarget.id.length - 1])] =
+					cellId.textContent;
+        turn += 1;
 }
 })
-
-const gameBoard = [
-    o, o, o,
-    o, o, o,
-    o, o, o
-];
-

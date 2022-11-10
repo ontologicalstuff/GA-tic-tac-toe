a36f5c2570e5bf3e46a476dbb827077f68a54355
ontologicalstuff <saorsa@tutamail.com>
2022-11-10 12:55:08 -0700

    finish cell const's and assigning of ids to cells

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // constants 
+const cell1 = document.querySelector('#cell-1');
+const cell2 = document.querySelector('#cell-2');
+const cell3 = document.querySelector('#cell-3');
+const cell4 = document.querySelector('#cell-4');
+const cell5 = document.querySelector('#cell-5');
+const cell6 = document.querySelector('#cell-6');
+const cell7 = document.querySelector('#cell-7');
+const cell8 = document.querySelector('#cell-8');
+const cell9 = document.querySelector('#cell-9');

842ebed55e4fe94ae634e209a6752d6ccac7a195
ontologicalstuff <saorsa@tutamail.com>
2022-11-10 13:21:18 -0700

    condense cellIds into a function

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 // constants 
-const cell1 = document.querySelector('#cell-1');
-const cell2 = document.querySelector('#cell-2');
-const cell3 = document.querySelector('#cell-3');
-const cell4 = document.querySelector('#cell-4');
-const cell5 = document.querySelector('#cell-5');
-const cell6 = document.querySelector('#cell-6');
-const cell7 = document.querySelector('#cell-7');
-const cell8 = document.querySelector('#cell-8');
-const cell9 = document.querySelector('#cell-9');
+const cellIds = document.querySelectorAll('div');
+
+cellIds.forEach((e) => {
+    e.onclick = (e) => {
+        const cellId = document.querySelector(`#${e.currentTarget.id}`);
+        cellId.textContent = 'X';
+}
+})

ccdc0e188ed6debc4d03027900a97279f71c8afd
ontologicalstuff <saorsa@tutamail.com>
2022-11-10 14:44:07 -0700

    add click disablers with pointerevents and write winning logic

--- main.js
+++ main.js
@@ -15,11 +15,26 @@ cellIds.forEach((e) => {
         const cellId = document.querySelector(`#${e.currentTarget.id}`);
         if (turn % 2 === 1) {
           cellId.textContent = 'X';
+          cellId.style.pointerEvents = 'none'
         } else {
-          cellId.textContent = 'O'
+          cellId.textContent = 'O';
+          cellId.style.pointerEvents = 'none'
         }      
         gameBoard[parseFloat(e.currentTarget.id[e.currentTarget.id.length - 1])] =
 					cellId.textContent;
         turn += 1;
 }
 })
+
+const checkForWinner = () => {
+    if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2] && typeof gameBoard[0] === 'string') {return;}
+    if (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5] && typeof gameBoard[3] === 'string') {return;}
+    if (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8] && typeof gameBoard[6] === 'string') {return;}
+    if (gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6] && typeof gameBoard[0] === 'string') {return;}
+    if (gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7] && typeof gameBoard[1] === 'string') {return;}
+    if (gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8] && typeof gameBoard[2] === 'string') {return;}
+	if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] && typeof gameBoard[0] === 'string') {return;}
+	if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && typeof gameBoard[2] === 'string') {return;}
+}
+
+checkForWinner();

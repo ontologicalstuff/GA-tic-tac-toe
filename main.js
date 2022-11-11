bbe38a8e76196e575407d349aacb61b3fdedd9c6
ontologicalstuff <saorsa@tutamail.com>
2022-11-10 18:00:08 -0700

    finish project

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 // constants 
 
-const gameBoard = [ 
+const holder =  {
+    b: [ 
     0, 0, 0, 
     0, 0, 0, 
     0, 0, 0
-];
-
-let turn = 1;
+],
+    turn: 1,
+};
 
 const mainBoard = document.querySelector('main');
 const cellIds = document.querySelectorAll('div');
+const newGameBtn = document.querySelector('button');
+const displayMessage = document.querySelector('h1');
 
 mainBoard.addEventListener('click', () => {
     if (checkForWinner()) {
@@ -20,26 +23,41 @@ mainBoard.addEventListener('click', () => {
 cellIds.forEach((e) => {
     e.onclick = (e) => {
         const cellId = document.querySelector(`#${e.currentTarget.id}`);
-        if (turn % 2 === 1) {
+        if (holder.turn % 2 === 1) {
           cellId.textContent = 'X';
-          cellId.style.pointerEvents = 'none'
+          cellId.style.pointerEvents = 'none';
+          displayMessage.textContent = `X's turn`
         } else {
           cellId.textContent = 'O';
-          cellId.style.pointerEvents = 'none'
+          cellId.style.pointerEvents = 'none';
+          displayMessage.textContent = `O's next`
         }      
-        gameBoard[parseFloat(e.currentTarget.id[e.currentTarget.id.length - 1])] =
+        holder.b[parseFloat(e.currentTarget.id[e.currentTarget.id.length - 1])] =
 					cellId.textContent;
-        turn += 1;
-}
+        holder.turn += 1;
+    }
 })
 
+
+
 function checkForWinner () {
-    if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2] && typeof gameBoard[0] === 'string') {return true;}
-    if (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5] && typeof gameBoard[3] === 'string') {return true;}
-    if (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8] && typeof gameBoard[6] === 'string') {return true;}
-    if (gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6] && typeof gameBoard[0] === 'string') {return true;}
-    if (gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7] && typeof gameBoard[1] === 'string') {return true;}
-    if (gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8] && typeof gameBoard[2] === 'string') {return true;}
-	if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] && typeof gameBoard[0] === 'string') {return true;}
-	if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && typeof gameBoard[2] === 'string') {return true;}
+    if (holder.b[0] === holder.b[1] && holder.b[0] === holder.b[2] && typeof holder.b[0] === 'string') {return true};
+    if (holder.b[3] === holder.b[4] && holder.b[3] === holder.b[5] && typeof holder.b[3] === 'string') {return true;}
+    if (holder.b[6] === holder.b[7] && holder.b[6] === holder.b[8] && typeof holder.b[6] === 'string') {return true;}
+    if (holder.b[0] === holder.b[3] && holder.b[0] === holder.b[6] && typeof holder.b[0] === 'string') {return true;}
+    if (holder.b[1] === holder.b[4] && holder.b[1] === holder.b[7] && typeof holder.b[1] === 'string') {return true;}
+    if (holder.b[2] === holder.b[5] && holder.b[2] === holder.b[8] && typeof holder.b[2] === 'string') {return true;}
+	if (holder.b[0] === holder.b[4] && holder.b[0] === holder.b[8] && typeof holder.b[0] === 'string') {return true;}
+	if (holder.b[2] === holder.b[4] && holder.b[2] === holder.b[6] && typeof holder.b[2] === 'string') {return true;}
+    if (holder.turn === 10) return displayMessage.textContent = `It's a tie!`;
 }
+
+newGameBtn.addEventListener('click', () => {
+	holder.b = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    cellIds.forEach((cell) => {
+        cell.textContent = '';
+        cell.style.pointerEvents = "auto";
+        holder.turn = 1;
+        displayMessage.textContent = '';
+    });
+});
